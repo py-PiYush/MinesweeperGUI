c89e23e8d0a481e3ee0c3fc957c41f3dd0758735
Piyush <impiyush016@gmail.com>
2022-05-12 16:33:08 +0530

    Added mines

--- cell.py
+++ cell.py
@@ -1,13 +1,22 @@
 from tkinter import Button
+import random
+import settings
 
 
 class Cell:
-    def __init__(self, is_mine=False):
+    ALL = []
+
+    def __init__(self, x, y, is_mine=False):
+        self.x = x
+        self.y = y
         self.is_mine = is_mine
         self.cell_btn_object = None
 
+        # Append the objects in Cell.ALL
+        Cell.ALL.append(self)
+
     def create_btn_object(self, location):
-        btn = Button(location, text="Text")
+        btn = Button(location, width=6, height=2, text=f"({self.x}, {self.y})")
         btn.bind("<Button-1>", self.left_click_actions)  # Left click
         btn.bind("<Button-3>", self.right_click_actions)  # right click
         self.cell_btn_object = btn
@@ -18,3 +27,12 @@ class Cell:
 
     def right_click_actions(self, event):
         print("i'm right click")
+
+    @staticmethod
+    def randomize_mines():
+        picked_cells = random.sample(Cell.ALL, settings.MINES)
+        for picked_cell in picked_cells:
+            picked_cell.is_mine = True
+
+    def __repr__(self) -> str:
+        return f"Cell({self.x}, {self.y})"
